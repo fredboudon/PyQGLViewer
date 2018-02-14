fc45e3d2acee8146fb45676a0df597a1900acb94
Frederic Boudon <frederic.boudon@cirad.fr>
2018-02-14 17:11:00 +0100

    add includes

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,4 +1,6 @@
-%PYTHON% configure.py
+%PYTHON% configure.py -I $PREFIX/include \
+-I $PREFIX/include/Qt -I $PREFIX/include/QtCore -I $PREFIX/include/QtOpenGL -I $PREFIX/include/QtXml -I $PREFIX/include/QtGUI \
+-L $PREFIX/lib
 
 if errorlevel 1 exit 1
 
--- conda/build.sh
+++ conda/build.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
+if [ "$(uname)" == "Darwin" ];
+then
+    export CC=clang
+    export CXX=clang++
 
-$PYTHON configure.py  
+    export MACOSX_VERSION_MIN=10.7
+    export CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
+    export LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+export LINKFLAGS="${LINKFLAGS} -stdlib=libc++ -std=c++11 "
+    export QMAKESPEC=macx-g++
+fi
+
+if [ "$(uname)" == "Linux" ];
+then
+    export QMAKESPEC=linux-g++
+fi
+
+
+$PYTHON configure.py  -I $PREFIX/include \
+-I $PREFIX/include/Qt -I $PREFIX/include/QtCore -I $PREFIX/include/QtOpenGL -I $PREFIX/include/QtXml -I $PREFIX/include/QtGUI \
+-L $PREFIX/lib --extra-cxxflags="${CXXFLAGS}" --extra-lflags="${LINKFLAGS}"
 
 make
 
