63c23f55f9245c7aec525a527cfb94d71a4a1598
Frederic Boudon <frederic.boudon@cirad.fr>
2018-03-01 16:38:21 +0100

    fix version of pyqt to have sip file

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,4 +1,4 @@
-%PYTHON% configureQt5.py --pyqt=PyQt4
+%PYTHON% configureQt5.py --verbose --pyqt=PyQt4
 
 if errorlevel 1 exit 1
 
@@ -6,5 +6,5 @@ nmake release
 
 if errorlevel 1 exit 1
 
-COPY src/python/PyQGLViewer.py %SP_DIR%
-COPY build/PyQGLViewerQt4/PyQGLViewerQt4.pyd %SP_DIR%
+COPY src\python\PyQGLViewer.py %SP_DIR%
+COPY build\PyQGLViewerQt4\PyQGLViewerQt4.pyd %SP_DIR%
