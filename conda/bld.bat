993e4af9a681f3a2651a7adb7ef62c5da2526b0a
Ethan Margaillan <ethan.margaillan@gmail.com>
2019-05-14 15:05:31 +0200

    Add Python 2 compatibility

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,4 +1,10 @@
-%PYTHON% configureQt5.py --verbose --pyqt=PyQt5
+if "%PY3K%"=="1" (
+    :: Python 3 -> MSVC 2015
+    %PYTHON% configureQt5.py --verbose --pyqt=PyQt5
+) else (
+    :: Python 2 -> MSVC 2008
+    %PYTHON% configureQt5.py --verbose --pyqt=PyQt5 --spec=win32-msvc2008
+)
 
 if errorlevel 1 exit 1
 
