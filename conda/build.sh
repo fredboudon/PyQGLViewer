0a323ee03ec8ad15b64cf47fc1a8301579709fe6
Frederic Boudon <frederic.boudon@cirad.fr>
2024-01-23 10:25:58 +0100

    remove py3.12. update cxxflags for build

--- conda/build.sh
+++ conda/build.sh
@@ -9,6 +9,14 @@ if [ "$(uname)" == "Linux" ];
 then
     export QMAKESPEC=linux-g++
     mv pyproject-linux.toml pyproject.toml
+
+    export PATH=${PWD}:${PATH}
+
+    SYSROOT_FLAGS="-L ${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64 -L ${BUILD_PREFIX}/${HOST}/sysroot/usr/lib"
+    export CFLAGS="$SYSROOT_FLAGS $CFLAGS"
+    export CXXFLAGS="$SYSROOT_FLAGS $CXXFLAGS"
+    export LDFLAGS="$SYSROOT_FLAGS $LDFLAGS"
+
 fi
 
 alias qmake='${CONDA_PREFIX}/bin/qmake'
